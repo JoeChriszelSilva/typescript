21e10f446810c01b24d7b2ea1bf199d3d0db0896
Joe Chriszel Silva <90300759+JoeChriszelSilva@users.noreply.github.com>
2025-08-28 10:41:20 -0300

    atualização aula9

---
+++ aula9/exercicio1.ts
@@ -0,0 +1,25 @@
+import leia = require('readline-sync');
+const fila = new Array<string>();
+let continuar : boolean = true;
+let opcao : number;
+
+do {
+    console.log("*************************************************");
+    console.log("");
+    console.log("      1 - Adicionar Cliente na Fila");
+    console.log("      2 - Listar todos os Clientes");
+    console.log("      3 - Retirar Cliente da Fila");
+    console.log("      0 - Sair");
+    console.log("");
+    console.log("*************************************************");
+    console.log("");
+    console.log("Entre com a opção desejada: ");
+    opcao = leia.questionInt();
+
+    switch (opcao) {
+        case 1:
+
+
+} while (continuar){
+    
+}
